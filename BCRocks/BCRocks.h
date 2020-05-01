ca3b3e92a702360d5492690a34997a072bf97b2f
gongzhen <gongzhengz@gmail.com>
2020-05-01 07:03:48 -0700

    init commit.

---
+++ BCRocks/BCRocks.h
@@ -0,0 +1,15 @@
+//
+//  BCRocks.h
+//  BCRocks
+//
+//  Created by Iska on 15/11/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCRocks : NSObject
+
+- (int)doStuff;
+
+@end
